f69a1f76f9fc0149f63ef51b0e8815c44af9ed2f
Mohamed Fazil <mfazil@hello-robot.com>
2024-07-01 23:33:59 -0700

    add joint qpos helper

---
+++ hello_helpers/src/hello_helpers/joint_qpos_conversion.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def get_Idx(tool_name):
+    if tool_name == 'eoa_wrist_dw3_tool_sg3':
+        return SE3_dw3_sg3_Idx
+    else:
+        raise ValueError('Undefined tool name in QposConversion.')
+
+class SE3_dw3_sg3_Idx:
+    LIFT = 1
+    ARM = 0
+    GRIPPER = 7
+    WRIST_ROLL = 4
+    WRIST_PITCH = 3
+    WRIST_YAW = 2
+    HEAD_PAN = 5
+    HEAD_TILT = 6
+    joints_N = 8
