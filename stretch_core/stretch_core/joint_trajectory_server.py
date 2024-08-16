2fa56f4227efcc79878491c23880317815a548c9
Mohamed Fazil <mfazil@hello-robot.com>
2024-08-16 10:56:35 -0700

    Invalidate fjt goal when in streaming position mode

--- stretch_core/stretch_core/joint_trajectory_server.py
+++ stretch_core/stretch_core/joint_trajectory_server.py
@@ -118,7 +118,8 @@ class JointTrajectoryAction(Node):
         if self.node.robot_mode not in ['position', 'trajectory', 'navigation']:
             self.node.robot_mode_rwlock.release_read()
             return self.error_callback(goal_handle, FollowJointTrajectory.Result.INVALID_GOAL, "Cannot execute goals while in mode={0}".format(self.node.robot_mode))
-        
+        if self.node.streaming_position_activated:
+            return self.error_callback(goal_handle, FollowJointTrajectory.Result.INVALID_GOAL, "Cannot execute goals Streaming Position Controller is activated.".format(self.node.robot_mode))
         if self.node.robot_mode in ['position','navigation']:
             # For now, ignore goal time and configuration tolerances.
             commanded_joint_names = goal.trajectory.joint_names
